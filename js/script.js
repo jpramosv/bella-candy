934812e63334dfe86129ca5b4801cb8900aae02a
Joao_Paulo_Ramos_Vieira <jpramos.comp@gmail.com>
2022-10-17 21:37:29 -0300

    added IIFE. Fixed #1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 (() => {
   const menuItens = document.querySelectorAll('.navigation__nav a[href^="#"]');
 
-  let chkbox = document.getElementById("navi-toggle");
-
   menuItens.forEach((item) => {
     item.addEventListener("click", scrollToIdOnClick);
   });
+  let chkbox = document.getElementById("navi-toggle");
 
   function scrollToIdOnClick() {
     chkbox.checked = false;
